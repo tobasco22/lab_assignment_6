dc8787d749064737e627927ea4ddbe80ab4fc7b2
Josh <jsasco22@gmail.com>
2023-02-23 13:13:27 -0500

    The outputs were incorrect due to me using the wrong formula in the int mid section. I also changed the > sign to a < sign for the seach to see if the right half has to be serched first. Also instead of using nested if statements I made them into stand alone if statements because I kept getting wrong outputs if I didnt change it. Lastly I used an else statement for if [mid]>value. I could have used an if staent but it just seemed easier to use an else statement since by the time that kicks in it is already known that [mid] isnt less than value.

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int search(int numbers[], int low, int high, int value) 
 {
+	//This checks if the array is empty and returns error code 1 if it is
 		if (low >= high){
-		int mid = low + (high - 1) / 2;
-
-		if(numbers[mid] == value)
-			return mid;
-
-			if(numbers[mid] > value)
-				return search(numbers, low, mid - 1, value);
-
-				return search(numbers, mid + 1, high, value);
+			return -1;
 		}
-	return -1;
+		//to find the mid point you need to get the average of the array so low + high divided by 2 gets the mid point
+		int mid = (low + high) / 2;
+
+		//if the value you are searching for is the mid point then it returns that
+		if(numbers[mid] == value){
+			return mid;}
+
+			//if the value you are searching for is greater than the mid point then the search is changed to 
+			//only look in the right half of the array from the mid point. This makes the seach faster since it does not need to 
+			//go through the entire array. The return recalls the search function but with the updated parameters.
+			if(numbers[mid] < value){
+				return search(numbers, mid + 1, high, value);}
+				//if the value is not bigger than the mid point hen this else function will kick in. It updates the search
+				//function to look on the left half of the array in relation to the mid point. The return recalls the 
+				//search funtion with the updated parameters.
+				else{
+					return search(numbers, low, mid - 1, value);
+		}
 }
 
 void printArray(int numbers[], int sz)
